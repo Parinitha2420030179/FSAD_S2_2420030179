d7756772d7595b2aafb6591d1cfdecad76b6c34b
ParinithaK179 <2420030179@klh.edu.in>
2026-03-04 21:00:22 +0530

    Add files via upload

---
+++ ProductCrudJPA-2/src/main/java/com/klh/ProductCrudJpa2Application.java
@@ -0,0 +1,13 @@
+package com.klh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCrudJpa2Application.class, args);
+	}
+
+}
